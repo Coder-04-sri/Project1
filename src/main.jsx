90e88014fd2023331ca027261edd063a42e12658
Coder-04-sri <vanisrikullappan2004@gmail.com>
2025-07-08 11:59:41 +0530

    git message added

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
+
 import { CartProvider } from './context/CartContext'; 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <CartProvider>
-    <HashRouter>
+    <BrowseRouter base='/Project1/'>
         <App />
-      </HashRouter>
+      </BrowseRouter>
     </CartProvider>
   </React.StrictMode>
 );
